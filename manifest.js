c706340e9c6c40143160294db771a875cc00abce
dmo18 <111906640+dmo18@users.noreply.github.com>
2026-06-24 22:28:33 -0400

    Show footer version from manifest

--- manifest.js
+++ manifest.js
@@ -10,3 +10,27 @@ const MYSUGYA_MANIFEST = [
     dataVersion: "12.3"
   }
 ];
+
+(function syncFooterVersionFromManifest() {
+  function currentVersion() {
+    var params = new URLSearchParams(window.location.search);
+    var moduleId = params.get("module") || (MYSUGYA_MANIFEST[0] && MYSUGYA_MANIFEST[0].id);
+    var mod = MYSUGYA_MANIFEST.find(function(item) { return item.id === moduleId; }) || MYSUGYA_MANIFEST[0];
+    return (mod && mod.dataVersion) || "";
+  }
+
+  function apply() {
+    var footerVersion = document.querySelector(".app-footer > span:first-child");
+    var version = currentVersion();
+    if (footerVersion && version) footerVersion.textContent = "Version " + version;
+  }
+
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", apply);
+  } else {
+    apply();
+  }
+
+  var observer = new MutationObserver(apply);
+  observer.observe(document.documentElement, { childList: true, subtree: true });
+})();
