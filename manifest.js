950f1c8e3925fdb16359268a75f249d868002a1e
dmo18 <111906640+dmo18@users.noreply.github.com>
2026-06-24 23:38:10 -0400

    Separate platform and data versions

--- manifest.js
+++ manifest.js
@@ -7,6 +7,7 @@ const MYSUGYA_MANIFEST = [
     dafRange: { first: "2a", last: "88a" },
     totalDaf: 173,
     dataScript: "modules/yoma/learning_data.js",
-    dataVersion: "13.1"
+    dataVersion: "12.27",
+    platformVersion: "13.1"
   }
 ];
